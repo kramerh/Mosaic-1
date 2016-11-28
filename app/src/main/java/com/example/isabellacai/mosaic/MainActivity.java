508ca0472034861c737b58c0a14c4ec18a5d1b1b
isabellacai <icai@stanford.edu>
2016-11-27 19:52:29 -0800

    added gridview and in process for adding stuff at the top

--- app/src/main/java/com/example/isabellacai/mosaic/MainActivity.java
+++ app/src/main/java/com/example/isabellacai/mosaic/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.isabellacai.mosaic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //switch activity to view that image based on it's position or id
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
