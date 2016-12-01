5d0736a15e2f48240ff510295e617ce412eca6bf
Heather <kramerh@stanford.edu>
2016-12-01 13:36:08 -0800

    idk if I updated anything but like here it is

--- app/src/main/java/com/example/isabellacai/mosaic/ImageAdapter.java
+++ app/src/main/java/com/example/isabellacai/mosaic/ImageAdapter.java
@@ -48,14 +48,14 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     // references to our images
-    public Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1
-    };
+//    public Integer[] mThumbIds = {
+//            R.drawable.sample_2, R.drawable.sample_3,
+//            R.drawable.sample_4, R.drawable.sample_5,
+//            R.drawable.sample_6, R.drawable.sample_7,
+//            R.drawable.sample_0, R.drawable.sample_1,
+//            R.drawable.sample_2, R.drawable.sample_3,
+//            R.drawable.sample_4, R.drawable.sample_5,
+//            R.drawable.sample_6, R.drawable.sample_7,
+//            R.drawable.sample_0, R.drawable.sample_1
+//    };
 }
--- app/src/main/java/com/example/isabellacai/mosaic/MainActivity.java
+++ app/src/main/java/com/example/isabellacai/mosaic/MainActivity.java
@@ -9,27 +9,18 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //add images to global variable
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
-
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                //switch activity to view that image based on it's position or id
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-
                 Intent intent = new Intent(MainActivity.this, ViewMosaicActivity.class);
                 intent.putExtra("position", position);
                 startActivity(intent);
