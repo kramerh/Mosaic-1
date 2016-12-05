49038b5aab3088549c278e1a1ccb25154a7b5a02
Heather <kramerh@stanford.edu>
2016-12-04 23:54:44 -0800

    the chihuahua stays in the box

--- app/src/main/java/com/example/isabellacai/mosaic/CanvasActivity.java
+++ app/src/main/java/com/example/isabellacai/mosaic/CanvasActivity.java
@@ -1,5 +1,6 @@
 package com.example.isabellacai.mosaic;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnDragListener;
 import android.view.View.OnLongClickListener;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -25,9 +27,11 @@ import android.widget.Toast;
 
 import stanford.androidlib.SimpleActivity;
 
+import static android.R.attr.id;
 import static com.example.isabellacai.mosaic.R.id.activity_canvas;
+import static com.example.isabellacai.mosaic.R.id.textView;
 
-public class CanvasActivity extends Activity {
+public class CanvasActivity extends SimpleActivity {
 
     private ImageView myImage;
     private static final String IMAGEVIEW_TAG = "Piece";
@@ -55,8 +59,6 @@ public class CanvasActivity extends Activity {
                 // called when the item is long-clicked
         @Override
         public boolean onLongClick(View view) {
-            // TODO Auto-generated method stub
-
             // create it from the object's tag
             ClipData.Item item = new ClipData.Item((CharSequence)view.getTag());
 
@@ -64,77 +66,77 @@ public class CanvasActivity extends Activity {
             ClipData data = new ClipData(view.getTag().toString(), mimeTypes, item);
             DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
             view.startDrag(data,shadowBuilder, view, 0);
-
             view.setVisibility(View.INVISIBLE);
             return true;
         }
     }
 
+
+
     class MyDragListener implements OnDragListener {
-        Drawable normalShape = getResources().getDrawable(R.drawable.sample_2, getTheme());
-        Drawable targetShape = getResources().getDrawable(R.drawable.sample_0, getTheme());
+        private boolean withinCanvas = true;
 
         @Override
         public boolean onDrag(View v, DragEvent event) {
-
             // Handles each of the expected events
             switch (event.getAction()) {
 
-                //signal for the start of a drag and drop operation.
-                case DragEvent.ACTION_DRAG_STARTED:
-                    // do nothing (ADD INFO POP UP?????)
-                    break;
-
-                //the drag point has entered the bounding box of the View
-                case DragEvent.ACTION_DRAG_ENTERED:
-                    //v.setBackground(targetShape);   //change the shape of the view
-                    break;
-
-                //the user has moved the drag shadow outside the bounding box of the View
-                case DragEvent.ACTION_DRAG_EXITED:
-                    //v.setBackground(R.drawable.);   //change the shape of the view back to normal
-                    break;
+//                //signal for the start of a drag and drop operation.
+//                case DragEvent.ACTION_DRAG_STARTED:
+//                    break;
+//                //the drag point has entered the bounding box of the View
+//                case DragEvent.ACTION_DRAG_ENTERED:
+//                    break;
+//
+//                //the user has moved the drag shadow outside the bounding box of the View
+//                case DragEvent.ACTION_DRAG_EXITED:
+//                   // v.setBackground(getResources().getDrawable(R.drawable.sample_0));
+//                    //v.setBackgroundColor(Color.BLUE); this is if it exits anything
+//                    break;
 
                 //drag shadow has been released,the drag point is within the bounding box of the View
                 case DragEvent.ACTION_DROP:
-                    // if the view is the bottomlinear, we accept the drag item
-                    if(v == findViewById(R.id.canvas)) {
-                        ImageView view = (ImageView) event.getLocalState();
-                        ViewGroup viewgroup = (ViewGroup) view.getParent();
-                        viewgroup.removeView(view);
-                        //change the text
-                        Toast.makeText(getApplicationContext(), "" + event.getX() + ", " + event.getY() + " | " + view.getWidth() + ", " + view.getHeight(),
-                                Toast.LENGTH_LONG).show();
-//                        TextView text = (TextView) v.findViewById(R.id.text);
-//                        text.setText("The item is dropped");
-                        //FrameLayout containView = (FrameLayout) v;
-                        FrameLayout containView = (FrameLayout) findViewById(R.id.canvas);
-                        //ImageView darg = (ImageView) event.getLocalState();
-                        view.setScaleX(100);
-                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
-                                view.getWidth(), view.getHeight());
-                        params.leftMargin =  0;//(int)event.getX();
-                        params.rightMargin = 0;//(int)event.getY();
+                    View canvas = findViewById(R.id.canvas);
+                    if(v == canvas) {
+                        float canvasX = getResources().getDimension(R.dimen.activity_horizontal_margin);
+                        float canvasY = getResources().getDimension(R.dimen.activity_horizontal_margin);
 
-                        containView.addView(view,params);
-                        view.setVisibility(View.VISIBLE);
-
-
+                        ImageView img = (ImageView) event.getLocalState();
+                        ViewGroup viewgroup = (ViewGroup) img.getParent();
+                        viewgroup.removeView(img);
+                        RelativeLayout containView = (RelativeLayout) v;
+                        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                              img.getWidth(), img.getHeight());
+                        containView.addView(img,params);
+                        float xOrigin = event.getX() - (img.getWidth()/2);
+                        float yOrigin = event.getY() - (img.getHeight()/2);
+                        if (xOrigin < 0)
+                            img.setX(0);
+                        else if (xOrigin + img.getWidth()> canvas.getWidth())
+                            img.setX(canvas.getWidth() - img.getWidth());
+                        else
+                            img.setX(xOrigin);
+                        if (yOrigin < 0)
+                            img.setY(0);
+                        else if (yOrigin + img.getHeight() > canvas.getHeight())
+                            img.setY(canvas.getHeight() - img.getHeight());
+                        else
+                            img.setY(yOrigin);
+                        img.setVisibility(View.VISIBLE);
                     } else {
                         View view = (View) event.getLocalState();
                         view.setVisibility(View.VISIBLE);
-                        Context context = getApplicationContext();
-                        Toast.makeText(context, "You can't drop the image here",
-                                Toast.LENGTH_LONG).show();
+                        toast("dropped - You can't drop this here", 1);
                         break;
                     }
                     break;
 
                 //the drag and drop operation has concluded.
                 case DragEvent.ACTION_DRAG_ENDED:
-                    //v.setBackground(normalShape);   //go back to normal shape
-                    //Toast.makeText(getApplicationContext(), "drag ended",
-                      //      Toast.LENGTH_LONG).show();
+                        View view = (View) event.getLocalState();
+                        view.setVisibility(View.VISIBLE);
+                        toast("ended - getResult is " + event.getResult(), 1);
+
                 default:
                     break;
             }
